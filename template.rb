b92b52ca89020747b91a271cfd35e9ce6d059d65
Kirill Bobykin <qelphybox@gmail.com>
2019-12-26 12:16:34 +0300

    separate templates

--- template.rb
+++ template.rb
@@ -8,118 +8,26 @@ def apply_template!
   setup_generators
   setup_databaseyml
 
-
   after_bundle do
+    # setup rspec
     run 'bundle exec rspec --init'
+    copy_file 'spec_rails_helper.rb', 'spec/rails_helper.rb'
+    template 'Dockerfile.dev.tt'
 
-    file 'spec/rails_helper.rb', <<~CODE
-      ENV['RAILS_ENV'] ||= 'test'
-      require 'spec_helper'
-      require File.expand_path('../../config/environment', __FILE__)
-      require 'rspec/rails'
-      require 'database_cleaner'
-
-      Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-      ActiveRecord::Migration.maintain_test_schema!
-
-      RSpec.configure do |config|
-        config.use_transactional_fixtures = false
-        config.include FactoryGirl::Syntax::Methods
-
-        config.before(:suite) do
-          DatabaseCleaner.clean_with(:truncation)
-        end
-
-        config.before(:each) do
-          DatabaseCleaner.strategy = :transaction
-        end
-
-        config.before(:each, js: true) do
-          DatabaseCleaner.strategy = :truncation
-        end
-
-        config.before(:each) do
-          DatabaseCleaner.start
-        end
-
-        config.after(:each) do
-          DatabaseCleaner.clean
-        end
-
-        config.before(:all) do
-          DatabaseCleaner.start
-        end
-
-        config.after(:all) do
-          DatabaseCleaner.clean
-        end
-
-        config.infer_spec_type_from_file_location!
-      end
-    CODE
-
-    file 'Dockerfile.dev', <<~CODE
-      FROM ruby:#{RUBY_VERSION}-alpine
-
-      ENV PROJECT_ROOT=/app
-      WORKDIR $PROJECT_ROOT
-
-      RUN apk update && \
-          apk add -u --no-cache --progress \
-          build-base cmake less \
-          postgresql-dev postgresql-client openssh git make
-
-      RUN gem install bundler
-
-      COPY Gemfile* $PROJECT_ROOT/
-      RUN bundle check || BUNDLE_FORCE_RUBY_PLATFORM=1 bundle install --retry 4
-    CODE
-
+    # prepare project structure
     run 'mkdir backend'
     backend_files = Dir.entries('.') - %w[. .. .git backend .gitignore]
     run "mv #{backend_files.join(' ')} -t backend"
     file 'frontend/README.md'
     file 'devops/README.md'
 
-    file 'docker-compose.yml', <<~YAML
-      version: '3'
-
-      services:
-        db:
-          image: postgres:12-alpine
-          environment:
-            PGPASSWORD_SUPERUSER: postgres
-            PGPASSWORD_ADMIN: postgres
-            PGPASSWORD_STANDBY: postgres
-          ports:
-            - "5432:5432"
-
-        backend:
-          build:
-            context: ./backend
-            dockerfile: Dockerfile.dev
-          command: ash -c "rm -f tmp/pids/server.pid && bundle exec rails s -b 0.0.0.0"
-          depends_on:
-            - db
-          ports:
-            - "8080:3000"
-          environment:
-            DATABASE_URL: postgresql://postgres@db/#{app_name}_development
-            REDIS_URL: redis://redis:6379/
-            RAILS_SERVE_STATIC_FILES: "true"
-            RAILS_LOG_TO_STDOUT: "true"
-            RAILS_ENV: development
-            HOST: 'http://localhost:8080'
-          volumes:
-            - ./backend:/app
-
-    YAML
-
+    template 'docker-compose.yml'
     template './Makefile.tt'
 
-    git :init
-    git add: '-- .'
-    git commit: "-a -m 'Initial commit'"
+    # git init, first commit
+    # git :init
+    # git add: '-- .'
+    # git commit: "-a -m 'Initial commit'"
   end
 end
 
@@ -158,27 +66,11 @@ def setup_generators
 end
 
 def setup_databaseyml
-  run 'rm config/database.yml'
-  file 'config/database.yml', <<~CODE
-    default: &default
-      adapter: postgresql
-      encoding: unicode
-      host: db
-      username: postgres
-      password:
-      pool: 5
-
-    development:
-      <<: *default
-      database: #{app_name}_development
-
-    test:
-      <<: *default
-      database: #{app_name}_test
-  CODE
+  # run 'rm config/database.yml'
+  template 'database.yml.tt', 'config/database.yml', force: true
 end
 
-# TODO: bad design, better will be make own Gemfile template
+# TODO: bad design, will be better to make own Gemfile template
 def remove_unwanted_gems
   %w(coffee-rails jbuilder tzinfo-data byebug).each do |unwanted_gem|
     gsub_file('Gemfile', /gem '#{unwanted_gem}'.*\n/, '')
